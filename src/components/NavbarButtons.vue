b8e9feb56656c3f9e54dc517c0db00e9b3540124
samzhang <samzhang0620@163.com>
2025-07-22 10:43:58 +0800

    移除推特展示和Buy Me A Coffee按钮（后续可还原此代码重新添加上去）

--- src/components/NavbarButtons.vue
+++ src/components/NavbarButtons.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { IconBrandGithub, IconBrandX, IconInfoCircle, IconMoon, IconSun } from '@tabler/icons-vue';
+import { IconBrandGithub, IconInfoCircle, IconMoon, IconSun } from '@tabler/icons-vue';
 import { useStyleStore } from '@/stores/style.store';
 
 const styleStore = useStyleStore();
@@ -17,19 +17,6 @@ const { isDarkTheme } = toRefs(styleStore);
       :aria-label="$t('home.nav.githubRepository')"
     >
       <n-icon size="25" :component="IconBrandGithub" />
-    </c-button>
-  </c-tooltip>
-
-  <c-tooltip :tooltip="$t('home.nav.twitterX')" position="bottom">
-    <c-button
-      circle
-      variant="text"
-      href="https://x.com/ittoolsdottech"
-      rel="noopener"
-      target="_blank"
-      :aria-label="$t('home.nav.twitterXAccount')"
-    >
-      <n-icon size="25" :component="IconBrandX" />
     </c-button>
   </c-tooltip>
 
